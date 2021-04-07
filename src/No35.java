4a18fb888219079c0a314ba931c494d5334a7f3b
jiaminglei <jiaminglei@blacklake.cn>
2021-04-07 19:12:30 +0800

    init project

---
+++ src/No35.java
@@ -0,0 +1,41 @@
+//搜索插入位置， leetcode第35题
+public class No35 {
+
+    public static void main(String[] args) {
+        int[] data = new int[]{1, 3, 5, 6};
+        int res = searchInsert(data, 7);
+        System.out.println(res);
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static int searchInsert2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
