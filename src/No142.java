5e958999b0d412f0de958cedb6951a5cb46a960f
jiaminglei <jiaminglei@blacklake.cn>
2021-04-20 18:56:35 +0800

    leetcode 142题

---
+++ src/No142.java
@@ -0,0 +1,48 @@
+/**
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ *
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+ *
+ * 说明：不允许修改给定的链表。
+ *
+ * 进阶：
+ *
+ * 你是否可以使用 O(1) 空间解决此题？
+ *
+ */
+public class No142 {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+            if (fast == slow) {
+                ListNode start = head;
+                while (fast != start) {
+                    start = start.next;
+                    fast = fast.next;
+                }
+                return start;
+            }
+        }
+        return null;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
