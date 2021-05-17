c933ece59a589cf5720f8004ea63d893edcf7370
jiaminglei <jiaminglei@blacklake.cn>
2021-05-17 19:31:45 +0800

    leetcode 225, 232

---
+++ src/栈和队列/No232.java
@@ -0,0 +1,62 @@
+package 栈和队列;
+
+import java.util.Stack;
+
+/**
+ * 232 用栈实现队列
+ *
+ * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+ *
+ * 实现 MyQueue 类：
+ *
+ * void push(int x) 将元素 x 推到队列的末尾
+ * int pop() 从队列的开头移除并返回元素
+ * int peek() 返回队列开头的元素
+ * boolean empty() 如果队列为空，返回 true ；否则，返回 false
+ *
+ * 说明：
+ *
+ * 你只能使用标准的栈操作 —— 也就是只有push to top,peek/pop from top,size, 和is empty操作是合法的。
+ * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+ *
+ */
+
+public class No232 {
+
+
+    class MyQueue {
+
+        Stack<Integer> stack1 = null;
+        Stack<Integer> stack2 = null;
+        public MyQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+
+        public void push(int x) {
+            stack1.push(x);
+        }
+
+        public int pop() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            return stack2.pop();
+        }
+
+        public int peek() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            return stack2.peek();
+        }
+
+        public boolean empty() {
+            return stack1.isEmpty() && stack2.isEmpty();
+        }
+    }
+}
