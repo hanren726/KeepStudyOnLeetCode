bb5b8c3f170b0168718bea8122c89dce91d74b99
jiaminglei <jiaminglei@blacklake.cn>
2021-05-19 18:48:29 +0800

    leetcode20 1047

---
+++ src/栈和队列/No1047.java
@@ -0,0 +1,46 @@
+package 栈和队列;
+
+import java.util.Stack;
+
+/**
+ *
+ * 1047. 删除字符串中的所有相邻重复项
+ *
+ * 给出由小写字母组成的字符串S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+ *
+ * 在 S 上反复执行重复项删除操作，直到无法继续删除。
+ *
+ * 在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+ *
+ *
+ * 示例：
+ *
+ * 输入："abbaca"
+ * 输出："ca"
+ * 解释：
+ * 例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+ *
+ */
+public class No1047 {
+
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (stack.isEmpty()) {
+                stack.push(cur);
+            } else {
+                if (cur != stack.peek()) {
+                    stack.push(cur);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        while (!stack.isEmpty()) {
+            res.insert(0, stack.pop());
+        }
+        return res.toString();
+    }
+}
