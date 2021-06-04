79e1fe3479f50869dedc30b36fb022a99b81c952
jiaminglei <jiaminglei@blacklake.cn>
2021-06-04 18:38:06 +0800

    leetcode 145

--- src/二叉树/No145.java
+++ src/二叉树/No145.java
@@ -1,5 +1,8 @@
 package 二叉树;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 145. 二叉树的后序遍历
  * 给定一个二叉树，返回它的 后序 遍历。
@@ -19,4 +22,38 @@ package 二叉树;
  */
 public class No145 {
 
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        postOrder(res, root);
+        return res;
+    }
+
+    public void postOrder(List<Integer> res, TreeNode node) {
+        if (node == null) return;
+        postOrder(res, node.left);
+        postOrder(res, node.right);
+        res.add(node.val);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode() {
+
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
