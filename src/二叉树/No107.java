b0649ceac39a1abb7dd999280ccc706b4c54a9e1
jiaminglei <jiaminglei@blacklake.cn>
2021-06-08 18:55:37 +0800

    leetcode 107

---
+++ src/二叉树/No107.java
@@ -0,0 +1,74 @@
+package 二叉树;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ *
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其自底向上的层序遍历为：
+ *
+ * [
+ *   [15,7],
+ *   [9,20],
+ *   [3]
+ * ]
+ *
+ */
+
+public class No107 {
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode() {
+
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int curLevelSize = queue.size();
+            List<Integer> curRes = new ArrayList<>();
+            for (int i = 0; i < curLevelSize; i++) {
+                TreeNode node = queue.poll();
+                curRes.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(0, curRes);
+        }
+        return res;
+    }
+}
