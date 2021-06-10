1c6c09224646ebedb035fb957b4ce9fe7a737a1b
jiaminglei <jiaminglei@blacklake.cn>
2021-06-10 19:30:29 +0800

    leetcode 199 637

---
+++ src/二叉树/No637.java
@@ -0,0 +1,74 @@
+package 二叉树;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ *给定一个非空二叉树, 返回一个由每层节点平均值组成的数组。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 输出：[3, 14.5, 11]
+ * 解释：
+ * 第 0 层的平均值是 3 ,  第1层是 14.5 , 第2层是 11 。因此返回 [3, 14.5, 11] 。
+ *
+ */
+public class No637 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode() {
+
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            double average = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (cur != null) {
+                    sum += cur.val;
+                    if (cur.left != null) {
+                        queue.offer(cur.left);
+                    }
+                    if (cur.right != null) {
+                        queue.offer(cur.right);
+                    }
+                }
+                average = sum / size;
+            }
+            res.add(average);
+        }
+        return res;
+    }
+}
