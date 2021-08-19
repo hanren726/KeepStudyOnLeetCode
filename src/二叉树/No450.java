51b68fe8f518968e62a1a3234db35cff3431aadd
jiaminglei <jiaminglei@blacklake.cn>
2021-08-19 19:47:04 +0800

    leetcdeo 450

--- src/二叉树/No450.java
+++ src/二叉树/No450.java
@@ -54,4 +54,40 @@ public class No450 {
         }
         return root;
     }
+
+    /**
+     * 第一种情况：没找到删除的节点，遍历到空节点直接返回了
+     * 找到删除的节点
+     * 第二种情况：左右孩子都为空（叶子节点），直接删除节点， 返回NULL为根节点
+     * 第三种情况：删除节点的左孩子为空，右孩子不为空，删除节点，右孩子补位，返回右孩子为根节点
+     * 第四种情况：删除节点的右孩子为空，左孩子不为空，删除节点，左孩子补位，返回左孩子为根节点
+     * 第五种情况：左右孩子节点都不为空，则将删除节点的左子树头结点（左孩子）放到删除节点的右子树的最左面节点的左孩子上，返回删除节点右孩子为新的根节点。
+     * @param root
+     * @param key
+     * @return
+     */
+    public TreeNode deleteNode1(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode cur = root.right;
+                while (cur.left != null) {
+                    cur = cur.left;
+                }
+                cur.left = root.left;
+                TreeNode temp = root;
+                root = temp.right;
+                return root;
+            }
+        }
+        return root;
+    }
 }
